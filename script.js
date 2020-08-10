5b1578790b3e851e7db2acc1c9c221e18702b1fa
Srikar2020 <68241974+Srikar2020@users.noreply.github.com>
2020-08-09 17:13:14 -0700

    I added the html and css and js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 // Assignment code here
 
+// start with box hidden
+document.getElementById("myDIV").style.display = "none";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var textarea = document.getElementById("password");
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  textarea.textContent = "kaialsh"
+  //passwordText.value = password;
 
 }
 
-function generatePassword() {
-
-  // return "password";
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", myFunction);
+
+
+
+
